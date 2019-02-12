ab2871cc03cddf3a3122fb6850c4ad482db7e67b
TSM Techø <trottimusbg@gmail.com>
2019-02-12 20:39:46 +0200

    [post-list-items.component] Add change status of loveIts and delete post functions.

--- src/app/components/post/post-list-item/post-list-item.component.ts
+++ src/app/components/post/post-list-item/post-list-item.component.ts
@@ -1,17 +1,37 @@
-import { Component, OnInit } from '@angular/core';
-import { faThumbsUp, faThumbsDown } from '@fortawesome/free-solid-svg-icons';
+import { Component, Input, OnInit } from '@angular/core';
+import { faHeart, faHeartBroken } from '@fortawesome/free-solid-svg-icons';
+import { Post } from '../../../core/models/post.model';
+import { PostService } from '../../../core/services/post.service';
 
 @Component({
-  selector: 'app-post-list-item',
-  templateUrl: './post-list-item.component.html',
-  styleUrls: ['./post-list-item.component.scss']
+    selector: 'app-post-list-item',
+    templateUrl: './post-list-item.component.html',
+    styleUrls: ['./post-list-item.component.scss']
 })
 export class PostListItemComponent implements OnInit {
-    like = faThumbsUp;
-    dislike = faThumbsDown;
+    @Input() post : Post;
+    date : string;
+    heart = faHeart;
+    heartBroke = faHeartBroken;
 
-  constructor() { }
+    constructor(private postService : PostService) {
+    }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+        this.date = new Date(this.post.creationDate).toLocaleDateString('en-US');
+    }
+
+    changePostStatus(type, post) {
+        if (type === 'increase') {
+            post.loveIts++;
+            this.postService.updatePost(post, post.id);
+        } else {
+            post.loveIts--;
+            this.postService.updatePost(post, post.id);
+        }
+    }
+
+    remove(id) {
+        this.postService.deletePost(id);
+    }
 }
