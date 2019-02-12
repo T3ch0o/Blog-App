81dce2e36d4b2856c726ba3d055f7a34f26380c7
TSM Techø <trottimusbg@gmail.com>
2019-02-12 16:00:45 +0200

    [app-routing.module] Add NewPostComponent to the routes.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,26 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { PostListComponent } from './components/post/post-list/post-list.component';
+import { NewPostComponent } from './components/post/new-post/new-post.component';
 
-const routes: Routes = [];
+const routes : Routes = [
+    {
+        path: 'posts',
+        component: PostListComponent
+    },
+    {
+        path: 'new',
+        component: NewPostComponent
+    },
+    {
+        path: '**',
+        redirectTo: 'posts'
+    }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
 })
-export class AppRoutingModule { }
+
+export class AppRoutingModule {}
