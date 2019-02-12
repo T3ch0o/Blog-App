3b0075249ab80d4e00db7152fd23b31d4714acde
TSM Techø <trottimusbg@gmail.com>
2019-02-12 20:32:30 +0200

    [new-post.component] Add FormGroup and FormBulder from ReactiveFormsModule.

--- src/app/components/post/new-post/new-post.component.ts
+++ src/app/components/post/new-post/new-post.component.ts
@@ -1,15 +1,33 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { PostService } from '../../../core/services/post.service';
+
 
 @Component({
-  selector: 'app-new-post',
-  templateUrl: './new-post.component.html',
-  styleUrls: ['./new-post.component.scss']
+    selector: 'app-new-post',
+    templateUrl: './new-post.component.html',
+    styleUrls: ['./new-post.component.scss']
 })
 export class NewPostComponent implements OnInit {
+    postForm : FormGroup;
 
-  constructor() { }
+    constructor(private fb : FormBuilder,
+                private postService : PostService,
+                private router : Router) {
+    }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+        this.postForm = this.fb.group({
+            title: '',
+            content: ''
+        });
+    }
+
+    onSubmit() {
+        const post = this.postForm.value;
+        this.postService.createPost(post)
+            .then(() => this.router.navigate(['posts']));
+    }
 
 }
