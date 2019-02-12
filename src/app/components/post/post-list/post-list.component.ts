edea2ce9269936e5157447718387400829c53b05
TSM Techø <trottimusbg@gmail.com>
2019-02-12 20:36:21 +0200

    [post-list.component] Inject postService and called getPosts function which get all the posts from firebase.

--- src/app/components/post/post-list/post-list.component.ts
+++ src/app/components/post/post-list/post-list.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { PostService } from '../../../core/services/post.service';
+import { Observable } from 'rxjs';
+import { Post } from '../../../core/models/post.model';
 
 @Component({
     selector: 'app-post-list',
@@ -6,10 +9,12 @@ import { Component, OnInit } from '@angular/core';
     styleUrls: ['./post-list.component.scss']
 })
 export class PostListComponent implements OnInit {
+    posts : Observable<Post[]>;
 
-    constructor() {
+    constructor(private postService : PostService) {
     }
 
     ngOnInit() {
+        this.posts = this.postService.getPosts();
     }
 }
