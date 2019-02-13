84fad331766147c7118a82758e3cddedf934c624
TSM Techø <trottimusbg@gmail.com>
2019-02-14 01:38:59 +0200

    [environment] Add firebaseConfig object.

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+    production: false,
+    firebaseConfig: {
+        apiKey: 'AIzaSyC5LDNY6_SmaFKavdFgFO1fOLlf2QSg9OU',
+        authDomain: 'blogapp-889c2.firebaseapp.com',
+        databaseURL: 'https://blogapp-889c2.firebaseio.com',
+        projectId: 'blogapp-889c2',
+        storageBucket: 'gs://blogapp-889c2.appspot.com',
+        messagingSenderId: '481430165460'
+    }
 };
 
 /*
